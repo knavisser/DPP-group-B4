87e3ccdcba4e670f633688c4821de0cc437b3675
jasperbruin <62376435+jasperbruin@users.noreply.github.com>
2022-11-23 15:29:53 +0100

    yes

--- simulate.c
+++ simulate.c
@@ -24,12 +24,8 @@ double *simulate(const int i_max, const int t_max, double *old_array,
                  double *current_array, double *next_array) {
 
     int process_Rank, size_Of_Cluster, t;
-    MPI_Status status;
-
-    // Tag: Number to distinguish different categories of messages.
-    // It is 0 by default because it is not used in this program.
     int tag = 0;
-
+    MPI_Status status;
     int rc = MPI_Init(NULL, NULL);
 
     if (rc != MPI_SUCCESS) {
@@ -37,87 +33,53 @@ double *simulate(const int i_max, const int t_max, double *old_array,
         MPI_Abort(MPI_COMM_WORLD, rc);
     }
 
-    // N tasks
+    // N tasks / Task ID
     MPI_Comm_size(MPI_COMM_WORLD, &size_Of_Cluster);
-
-    // This is the task ID.
     MPI_Comm_rank(MPI_COMM_WORLD, &process_Rank);
 
-    // n_local = npoints / numtasks: n_local represents the number of points
     int n_local = (int) ceil((double) i_max / size_Of_Cluster);
 
-    // double cur[n_local + 2] double new[n_local + 2]  double old[n_local + 2];
     double *cur = malloc((n_local + 2) * sizeof(double));
     double *new = malloc((n_local + 2) * sizeof(double));
     double *old = malloc((n_local + 2) * sizeof(double));
 
-
-
-    // special left treatment: left_neighbor = task_id - 1
-    // special right treatment: right_neighbor = task_id + 1
     int left_neightbor = process_Rank - 1;
     int right_neightbor = process_Rank + 1;
 
     // MASTER Process
     if (process_Rank == 0) {
         for (t = 1; t < size_Of_Cluster - 1; t++) {
-            for (int i = 1; i < n_local; i++) {
+            for (int i = 0; i < n_local; i++) {
                 cur[i] = current_array[i];
-            }
-
-            MPI_Send(cur, n_local, MPI_DOUBLE, t, tag, MPI_COMM_WORLD);
-
-            for (int i = 1; i < n_local; i++) {
                 old[i] = old_array[i];
             }
+            MPI_Send(cur, n_local, MPI_DOUBLE, t, tag, MPI_COMM_WORLD);
             MPI_Send(old, n_local, MPI_DOUBLE, t, tag, MPI_COMM_WORLD);
-
         }
 
-        // initialize (cur[1:n_local]
-        //printf("initialize (cur[1:n_local] for process %d \n", process_Rank);
-        for (int i = 1; i < n_local; i++) {
+        for (int i = 0; i < n_local; i++) {
             cur[i] = current_array[i];
-        }
-
-        // initialize (old[1:n_local]
-        //printf("initialize (old[1:n_local] for process %d \n", process_Rank);
-        for (int i = 1; i < n_local; i++) {
             old[i] = old_array[i];
         }
-
     }
-    // Worker processes
+
     else {
+        MPI_Recv(old, n_local, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
         MPI_Recv(cur, n_local, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
-        MPI_Recv(old, n_local, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
     }
-
-    //printf("process_Rank: %d, size_Of_Cluster: %d, n_local: %d, left_neightbor: %d, right_neightbor: %d \n", process_Rank, size_Of_Cluster, n_local, left_neightbor, right_neightbor);
-
-
 
     // loop over t-max
-    printf("n_local %d\n", n_local);
     for (t = 1; t < t_max; t++) {
-        printf("t = %d for process %d\n", t, process_Rank);
-
         if (left_neightbor != -1) {
-            printf("send (t, cur[1] for process %d\n", process_Rank);
-            fflush(0);
             MPI_Send(&cur[1], 1, MPI_DOUBLE, left_neightbor, tag, MPI_COMM_WORLD);
+            MPI_Recv(&cur[n_local + 1], 1, MPI_DOUBLE, right_neightbor, tag, MPI_COMM_WORLD, &status);
         }
-
-        MPI_Recv(&cur[n_local + 1], 1, MPI_DOUBLE, right_neightbor, tag, MPI_COMM_WORLD, &status);
 
         if (right_neightbor != size_Of_Cluster) {
-            printf("send (t, cur[n_local] for process %d\n", process_Rank);
             MPI_Send(&cur[n_local], 1, MPI_DOUBLE, right_neightbor, tag, MPI_COMM_WORLD);
+            MPI_Recv(&cur[0], 1, MPI_DOUBLE, left_neightbor, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
         }
-
-
-        // cur[0] = receive (left_neighbor)
-        MPI_Recv(&cur[0], 1, MPI_DOUBLE, left_neightbor, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
         printf("Simulating wave for process %d\n", process_Rank);
         for (int i = 1; i < n_local + 1; i++) {
@@ -125,30 +87,25 @@ double *simulate(const int i_max, const int t_max, double *old_array,
         }
 
         // rotate the buffers
-        double *temp = old_array;
-        old_array = current_array;
-        current_array = next_array;
-        next_array = temp;
+        double *temp = old;
+        old = cur;
+        cur = new;
+        new = temp;
 
         if (process_Rank > 0) {
             MPI_Send(cur, n_local, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
         }
+
         else {
-            // write (file, cur[1:n_local])
-            printf("Process %d: cur[1] = %f, old[1] = %f" , process_Rank, cur[1], old[1]);
-
             for (int i = 1 ; i < n_local - 1; i++) {
-                //cur[1:n_local] = receive (i)
                 MPI_Recv(cur, n_local, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-                //write (file, cur[1:n_local])
-                printf("Process %d: cur[1] = %f, old[1] = %f" , process_Rank, cur[1], old[1]);
             }
         }
     }
 
-    free(old_array);
-    free(current_array);
+    free(cur);
+    free(new);
+    free(old);
 
     // finalize MPI
     MPI_Finalize();
